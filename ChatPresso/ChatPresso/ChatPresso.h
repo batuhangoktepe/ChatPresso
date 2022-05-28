79062004cbfd150b9c2d111df17ce303ba52962b
Batuhan <guardmi15@gmail.com>
2022-05-28 13:48:42 +0300

    Initial commit

---
+++ ChatPresso/ChatPresso/ChatPresso.h
@@ -0,0 +1,18 @@
+//
+//  ChatPresso.h
+//  ChatPresso
+//
+//  Created by Batuhan Göktepe on 28.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatPresso.
+FOUNDATION_EXPORT double ChatPressoVersionNumber;
+
+//! Project version string for ChatPresso.
+FOUNDATION_EXPORT const unsigned char ChatPressoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatPresso/PublicHeader.h>
+
+
